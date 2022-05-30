d37e63a1ded3c6d86098bfd6dc5ad95c079c47ea
ivanafonin <afonin.ivan02@yandex.ru>
2022-05-30 11:28:49 +0200

    enhance pop up layout

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Input = ({ onChange, value, placeholder, backgroundImage }) => {
-	return (
-		<CustomInput
-			backgroundImage={backgroundImage}
-			placeholder={placeholder}
-			onChange={onChange}
-			value={value}
-		/>
-	);
+export const Input = ({
+  onChange,
+  value,
+  placeholder,
+  backgroundImage,
+}) => {
+  return (
+    <CustomInput
+      backgroundImage={backgroundImage}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+    />
+  );
 };
 
 const CustomInput = styled.input`
-	background: ${(props) => (props.active ? '#546EE5' : '#34395A')};
-	cursor: pointer;
-	font-size: ${(props) => props.fontSize};
-	text-align: center;
-	transition: 0.3s all;
-	line-height: 48px;
-	border-radius: 4px;
-	background-image: ${(props) =>
-		props.backgroundImage ? `url(${props.backgroundImage})` : 'none'};
+  color: #ffffff;
+  border: 0 solid white;
+  height: 40px;
+  display: block;
+  outline: none;
+  margin-top: 16px;
+  padding-left: 36px;
+  border-radius: 4px;
+  background: #34395a;
+  background-repeat: no-repeat;
+  background-position: 12px center;
+  font-size: ${(props) => props.fontSize};
+  line-height: 48px;
+  background-image: ${(props) =>
+    props.backgroundImage ? `url(${props.backgroundImage})` : 'none'};
 `;
--- src/pages/Login/LoginModal.js
+++ src/pages/Login/LoginModal.js
@@ -1,50 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { chooseMarket } from '../../slices/marketSlice';
-import styled from 'styled-components';
 import { Button } from '../../components/ui/Button';
+import { Input } from '../../components/ui/Input';
+import lockIcon from '../../assets/images/lockIcon.png';
+import usernameIcon from '../../assets/images/usernameIcon.png';
+
+import styled from 'styled-components';
+import { Text } from '../../components/ui/Text';
+import { Switcher } from '../../components/ui/Switcher';
 
 export const LoginModal = () => {
-	const market = useSelector((state) => state.market.country);
-	const dispatch = useDispatch();
+  const [loginForm, setLoginForm] = useState({
+    username: '',
+    password: '',
+  });
+  const market = useSelector((state) => state.market.country);
+  const dispatch = useDispatch();
 
-	const handleCloseModal = () => {
-		dispatch(chooseMarket(''));
-	};
+  const handleCloseModal = () => {
+    dispatch(chooseMarket(''));
+  };
 
-	return (
-		<Modal open={!!market}>
-			<p>Login</p>
-			<div>
-				<p>Info Type</p>
-				<div>
-					<div>Box strategies</div>
-					<div>Stock + option strategies</div>
-				</div>
-				<input />
-				<input />
-				<ButtonContainer>
-					<Button text="Apply" active />
-					<Button onClick={handleCloseModal} text="Cancel" />
-				</ButtonContainer>
-			</div>
-		</Modal>
-	);
+  const handleLoginInput = (key, value) => {
+    setLoginForm({ ...loginForm, [key]: value });
+  };
+
+  return (
+    <Modal open={!!market}>
+      <LoginText>Login</LoginText>
+      <div>
+        <Text>Info Type</Text>
+        <Switcher />
+        <InputContainer>
+          <Input
+            value={loginForm.username}
+            onChange={(e) =>
+              handleLoginInput('username', e.target.value)
+            }
+            placeholder="Username"
+            backgroundImage={usernameIcon}
+          />
+          <Input
+            value={loginForm.password}
+            onChange={(e) =>
+              handleLoginInput('password', e.target.value)
+            }
+            placeholder="Password"
+            backgroundImage={lockIcon}
+          />
+        </InputContainer>
+        <ErrorText />
+
+        <ButtonContainer>
+          <ApplyButton text="Apply" active />
+          <Button onClick={handleCloseModal} text="Cancel" />
+        </ButtonContainer>
+      </div>
+    </Modal>
+  );
 };
 
 const Modal = styled.div`
-	display: ${(props) => (props.open ? 'block' : 'none')};
-	width: 600px;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	transition: 1.5s all;
-	padding: 24px;
-	background: ${(props) => props.theme.palette.primary.main};
+  //display: ${(props) => (props.open ? 'block' : 'none')};
+  display: block;
+  width: 600px;
+  position: absolute;
+  top: ${(props) => (props.open ? '50%' : '150%')};
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: 1.5s all;
+  padding: 24px;
+  background: ${(props) => props.theme.palette.primary.main};
+  z-index: 3;
 `;
 
 const ButtonContainer = styled.div`
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ErrorText = styled.p`
+  color: #eb001b;
+  margin-top: 16px;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ApplyButton = styled(Button)`
+  margin: 16px 0;
+`;
+
+const LoginText = styled.h3`
+  font-size: 24px;
+  text-align: center;
+  line-height: 32px;
+  font-weight: 400;
 `;
