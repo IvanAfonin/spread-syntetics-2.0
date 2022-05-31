c40d5acc9314e3c61287a147727ab92e8c8dad25
ivanafonin <afonin.ivan02@yandex.ru>
2022-05-31 15:42:15 +0200

    Test CI and spec for login page

--- cypress/integration/login/checkLoginModal.spec.js
+++ cypress/integration/login/checkLoginModal.spec.js
@@ -1,5 +1,15 @@
 describe('Check login elements', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Check login page', () => {
+    cy.contains('Choose your intrested region').should('be.visible');
+    cy.get('#europeSVG').should('be.visible');
+    cy.get('#usaSVG').should('be.visible');
+  });
+
   it('Check modal availability', () => {
-    cy.get('Choose your intrested region').should('be.visible');
+    cy.get('#europeSVG').click();
+    cy.contains('Apply').should('be.visible');
   });
 });
