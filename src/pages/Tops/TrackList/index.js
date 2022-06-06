df444e65d3274b9cc4650ca4bfb1414501697547
ivanafonin <afonin.ivan02@yandex.ru>
2022-06-06 16:46:41 +0200

    Changes tracklist structure

--- src/pages/Tops/TrackList/index.js
+++ src/pages/Tops/TrackList/index.js
@@ -1,94 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
-import { Card } from '../../../components/ui/Card';
-import { STRATEGY } from '../../../constants/Strategy';
-import { TracklistRow } from './TrackListRow';
+import { Tracklist } from './Tracklist';
+import {
+  TRACKLIST_TYPE_LONG,
+  TRACKLIST_TYPE_SHORT,
+} from '../../../constants/TrackLists';
 
-export const Tracklist = ({ title, data, tracklistType }) => {
-  const strategy = useSelector((state) => state.market.strategy);
+export const TracklistSection = () => {
+  const trackListShort = useSelector(
+    (state) => state.tops.tracklists.short
+  );
+  const trackListLong = useSelector(
+    (state) => state.tops.tracklists.long
+  );
+
   return (
-    <StyledCard>
-      <TrackListTitle>{title}</TrackListTitle>
-      <TrackListTopRow>
-        <TrackListCell>№</TrackListCell>
-        <TrackListCell>Stocks</TrackListCell>
-        <TrackListCell
-        // className={
-        //   classes.tracklist__content_cell + ' ' + calculationType
-        // }
-        >
-          Strike 1
-        </TrackListCell>
-        {strategy === STRATEGY.BOX && (
-          <TrackListCell>Strike 2</TrackListCell>
-        )}
-        <TrackListCell
-          isAlignedCenter={strategy === STRATEGY.STOCKANDOPTIONS}
-        >
-          Mark
-        </TrackListCell>
-      </TrackListTopRow>
-      {data.map((row, index) => (
-        <TracklistRow
-          tracklistType={tracklistType}
-          index={index}
-          data={row}
-          key={row + index}
-        />
-      ))}
-    </StyledCard>
+    <TrackListContainer>
+      <Tracklist
+        tracklistType={TRACKLIST_TYPE_SHORT}
+        data={trackListShort}
+        title="Short"
+      />
+      <Tracklist
+        tracklistType={TRACKLIST_TYPE_LONG}
+        data={trackListLong}
+        title="Long"
+      />
+    </TrackListContainer>
   );
 };
 
-const TrackListTitle = styled.p`
-  font-size: 16px;
-  height: 24px;
-  line-height: 24px;
-  margin-bottom: ${(props) => props.theme.spacing(2)}px;
-`;
-
-const TrackListCell = styled.div`
-  font-size: 14px;
-  text-align: ${(props) =>
-    props.isAlignedCenter ? 'center' : 'left'};
-  &:nth-child(1) {
-    width: 24px;
-  }
-  &:nth-child(2) {
-    width: 80px;
-  }
-  &:nth-child(3),
-  &:nth-child(4),
-  &:nth-child(5) {
-    width: 64px;
-  }
-  &:nth-child(5),
-  &:nth-child(1) {
-    text-align: center;
-  }
-  &.stocks.options.strategies {
-    width: 144px;
-  }
-`;
-
-const TrackListTopRow = styled.div`
+const TrackListContainer = styled.div`
   display: flex;
+  width: 55%;
   justify-content: space-between;
-  align-items: center;
-  margin-bottom: ${(props) => props.theme.spacing(1)}px;
-  :nth-child(1) {
-    color: ${(props) => props.theme.palette.text.secondary};
-    margin-bottom: ${(props) => props.theme.spacing(1.5)}px;
-  }
-`;
-
-const StyledCard = styled(Card)`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-
-  :nth-child(2) {
-    margin-left: ${(props) => props.theme.spacing(1)}px;
-  }
 `;
--- src/pages/Tops/index.js
+++ src/pages/Tops/index.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import { Tracklist } from './TrackList';
-import styled from 'styled-components';
-import { useSelector } from 'react-redux';
-import {
-  TRACKLIST_TYPE_LONG,
-  TRACKLIST_TYPE_SHORT,
-} from '../../constants/TrackLists';
+import { TracklistSection } from './TrackList';
 
 export const Tops = () => {
-  const trackListShort = useSelector(
-    (state) => state.tops.tracklists.short
-  );
-  const trackListLong = useSelector(
-    (state) => state.tops.tracklists.long
-  );
-
   return (
-    <TrackListContainer>
-      <Tracklist
-        tracklistType={TRACKLIST_TYPE_SHORT}
-        data={trackListShort}
-        title="Short"
-      />
-      <Tracklist
-        tracklistType={TRACKLIST_TYPE_LONG}
-        data={trackListLong}
-        title="Long"
-      />
-    </TrackListContainer>
+    <>
+      <TracklistSection />
+    </>
   );
 };
-
-const TrackListContainer = styled.div`
-  display: flex;
-  width: 55%;
-  justify-content: space-between;
-`;
